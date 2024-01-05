60c0dd711af33a58f4967d4b6e6a48ac67e64499
DoA Voices <100880439+DoAPandora@users.noreply.github.com>
2024-01-04 23:18:12 -0500

    Fix Leaderboard Data

--- src/BeatLeader.cpp
+++ src/BeatLeader.cpp
@@ -10,7 +10,7 @@ Player::Player(rapidjson::Value const& userModInterface) {
     role = userModInterface["role"].GetString();
 
     // For standard context and players from v3/scores (where contextExtension is null cause we request for one context) we use the main player
-    int currentContext = getModConfig().Context.GetValue();
+    int currentContext = getConfig().Context.GetValue();
     std::optional<rapidjson::GenericArray<true, rapidjson::Value>> contextExtensions = userModInterface.HasMember("contextExtensions") && !userModInterface["contextExtensions"].IsNull() ? userModInterface["contextExtensions"].GetArray() : std::optional<rapidjson::GenericArray<true, rapidjson::Value>>();
     // If we are Standard Context or we have no contexts or our selected context is not in contextextensions we use the normal rank. Else we use the correct context extension rank
     rapidjson::Value const& contextRank = currentContext == 0 || !contextExtensions || currentContext - 1 >= contextExtensions.value().Size() ? userModInterface : contextExtensions.value()[currentContext - 1];
