30ad5c98b801503a87493c428c19cf15ff9db261
DoA Voices <100880439+DoAPandora@users.noreply.github.com>
2024-01-05 18:08:01 -0500

    Add files via upload

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,6 @@
 #include "main.hpp"
-
 #include "include/ScoreSaberData.hpp"
 #include "include/BeatLeaderData.hpp"
-
-#include "GlobalNamespace/MainMenuViewController.hpp"
-#include "UnityEngine/UI/Button.hpp"
-#include "UnityEngine/GameObject.hpp"
-#include "HMUI/CurvedTextMeshPro.hpp"
-
-MAKE_HOOK_MATCH(MainMenuUIHook, &GlobalNamespace::MainMenuViewController::DidActivate, void,
- GlobalNamespace::MainMenuViewController* self, bool firstActivation, bool addedToHierarchy, bool screenSystemEnabling) {
-MainMenuUIHook(self, firstActivation, addedToHierarchy, screenSystemEnabling);
-
-UnityEngine::UI::Button* soloMenuButton = self->soloButton;
-    UnityEngine::GameObject* gameObject = soloMenuButton->get_gameObject();
-    HMUI::CurvedTextMeshPro* soloMenuText = gameObject->GetComponentInChildren<HMUI::CurvedTextMeshPro*>();
-
-    // Set the text to "Score Viewer"
-    soloMenuText->SetText("Score Viewer");
 
 static ModInfo modInfo; // Stores the ID and version of our mod, and is sent to the modloader upon startup
 
@@ -48,15 +31,7 @@ extern "C" void setup(ModInfo& info) {
 extern "C" void load() {
     il2cpp_functions::Init();
 
-// Regsiter our Settings
-    QuestUI::Register::RegisterModSettingsViewController(modInfo, DidActivate);
-
     getLogger().info("Installing hooks...");
-    INSTALL_HOOK(getLogger(), ActivateFlowCoordinatorHook);
-    INSTALL_HOOK(getLogger(), FixedUpdateHook);
-    INSTALL_HOOK(getLogger(), LevelSelectionNavigationControllerDidActivate);
-    INSTALL_HOOK(getLogger(), LevelSelectionNavigationControllerDidDeactivate);
-    INSTALL_HOOK(getLogger(), MainMenuUIHook);
-    getLogger().info("Installed all hooks!");
     
+    getLogger().info("Installed all hooks!");
 }
