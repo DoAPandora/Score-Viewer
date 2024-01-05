b2a2883065aa6d6e9f004d6eaf6a339ac57f79be
DoA Voices <100880439+DoAPandora@users.noreply.github.com>
2024-01-04 23:14:43 -0500

    Fix Leaderboard Data

--- src/BeatLeader.cpp
+++ src/BeatLeader.cpp
@@ -18,48 +18,4 @@ Player::Player(rapidjson::Value const& userModInterface) {
     rank = contextRank["rank"].GetInt();
     countryRank = contextRank["countryRank"].GetInt();
     pp = contextRank["pp"].GetFloat();
-
-    //auto clansList = userModInterface["clans"].GetArray();
-    //for (int index = 0; index < (int)clansList.Size(); ++index) {
-        //auto const& clan = clansList[index];
-    }
-
-    if (userModInterface.HasMember("friends") && !userModInterface["friends"].IsNull()) {
-        auto friendsList = userModInterface["friends"].GetArray();
-        for (int index = 0; index < (int)friendsList.Size(); ++index) {
-            auto const& friendId = friendsList[index].GetString();
-            friends.push_back(friendId);
-        }
-    }
-
-    if (userModInterface.HasMember("socials") && !userModInterface["socials"].IsNull()) {
-        auto socialsList = userModInterface["socials"].GetArray();
-        for (int index = 0; index < (int)socialsList.Size(); ++index) {
-            auto const& social = socialsList[index];
-            socials.push_back(Social(social));
-        }
-    }
-
-    if (userModInterface.HasMember("profileSettings")) {
-        profileSettings.emplace(ProfileSettings(userModInterface["profileSettings"].GetObject()));
-    }
-}
-
-void Player::SetHistory(rapidjson::Value const& history) {
-    lastRank = history["rank"].GetInt();
-    lastCountryRank = history["countryRank"].GetInt();
-    lastPP = history["pp"].GetFloat();
-}
-
-Social::Social(rapidjson::Value const& document) {
-    service = document["service"].GetString();
-    link = document["link"].IsNull() ? "" : document["link"].GetString();
-    user = document["user"].IsNull() ? "" : document["user"].GetString();
-}
-
-ProfileSettings::ProfileSettings(rapidjson::Value const& document) {
-    message = document["message"].IsNull() ? "" : document["message"].GetString();
-    effectName = document["effectName"].IsNull() ? "" : document["effectName"].GetString();
-    hue = document["hue"].GetInt();
-    saturation = document["saturation"].GetFloat();
 }
