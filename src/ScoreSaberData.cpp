09b4dba78ca9ec07cc4be8a1ef4bf7a642ef6131
DoA Voices <100880439+DoAPandora@users.noreply.github.com>
2024-01-04 22:11:55 -0500

    Upload Base

---
+++ src/ScoreSaberData.cpp
@@ -0,0 +1,37 @@
+#include "beatsaber-hook/shared/utils/utils.h"
+
+namespace ScoreSaber::Data
+{
+    LeaderboardPlayer::LeaderboardPlayer(const rapidjson::Value&& value)
+    {
+        auto idItr = value.FindMember("id");
+        if (idItr != value.MemberEnd())
+        {
+            id = std::make_optional(std::string(idItr->value.GetString()));
+        }
+
+        auto nameItr = value.FindMember("name");
+        if (nameItr != value.MemberEnd())
+        {
+            name = std::make_optional(to_utf16(std::string(nameItr->value.GetString())));
+        }
+
+        profilePicture = value["profilePicture"].GetString();
+
+        auto countryItr = value.FindMember("country");
+        if (countryItr != value.MemberEnd())
+        {
+            country = std::make_optional(std::string(countryItr->value.GetString()));
+        }
+
+        auto permissionsItr = value.FindMember("permissions");
+        if (permissionsItr != value.MemberEnd())
+        {
+            permissions = std::make_optional(int(permissionsItr->value.GetInt()));
+        }
+
+        auto roleItr = value.FindMember("role");
+        if (roleItr != value.MemberEnd() && roleItr->value.IsString())
+            role = std::make_optional(std::string(roleItr->value.GetString()));
+    }
+}
