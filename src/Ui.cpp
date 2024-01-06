53e1afecb93b31aa72fa35b16d3a3a567fbd0245
DoA Voices <100880439+DoAPandora@users.noreply.github.com>
2024-01-05 21:58:37 -0500

    Please Work

--- src/Ui.cpp
+++ src/Ui.cpp
@@ -1,14 +1,57 @@
+#pragma once
 
-#include "main.hpp"
-#include "assets.hpp"
+#include "custom-types/shared/macros.hpp"
+#include "UnityEngine/MonoBehaviour.hpp"
 
-#include "bsml/shared/BSML/Components/ClickableImage.hpp"
-#include "questui/shared/QuestUI.hpp"
+#ifndef DECLARE_INSTANCE_FIELD_TYPENAME
+#define DECLARE_INSTANCE_FIELD_TYPENAME(type_, ScoreViewer_, namezpace_, klass_)   \
+private:                                                                    \
+struct ___FieldRegistrator_##name_ : ::custom_types::FieldRegistrator {     \
+    ___FieldRegistrator_##name_() {                                         \
+        ___TargetType::___TypeRegistration::addField(this);                 \
+    }                                                                       \
+    constexpr const char* name() const override {                           \
+        return #name_;                                                      \
+    }                                                                       \
+    const Il2CppType* type() const override {                               \
+        ::il2cpp_functions::Init();                                         \
+        return ::il2cpp_functions::class_get_type(                          \
+            il2cpp_utils::GetClassFromName(namezpace_, klass_)              \
+        );                                                                  \
+    }                                                                       \
+    constexpr uint16_t fieldAttributes() const override {                   \
+        return FIELD_ATTRIBUTE_PUBLIC;                                      \
+    }                                                                       \
+    constexpr size_t size() const override {                                \
+        return sizeof(type_);                                               \
+    }                                                                       \
+    int32_t offset() const override {                                       \
+        return offsetof(___TargetType, name_);                              \
+    }                                                                       \
+};                                                                          \
+static inline ___FieldRegistrator_##name_ ___##name_##_FieldRegistrator;    \
+public:                                                                     \
+type_ name_
+#endif
 
-void DidActivate(HMUI::ViewController* self, bool firstActivation, bool addedToHierarchy, bool screenSystemEnabling) {
-    // Create our UI elements only when shown for the first time.
-    if(firstActivation) {
-        
-        BSML::ClickableImage* CreateClickableImage(const TransformWrapper& parent, UnityEngine::Sprite* sprite, std::function<void()> onClick = nullptr, UnityEngine::Vector2 anchoredPosition = {-70, 30}, UnityEngine::Vector2 sizeDelta = {9.0f, 9.0f}
-    }
+namespace BSML {
+    class TabSelector;
 }
+
+DECLARE_CLASS_CODEGEN(BSML, Tab, UnityEngine::MonoBehaviour,
+    DECLARE_INSTANCE_FIELD_TYPENAME(TabSelector*, selector, "BSML", "TabSelector");
+    DECLARE_INSTANCE_FIELD(StringW, tabName);
+    DECLARE_INSTANCE_FIELD(StringW, tabKey);
+    DECLARE_INSTANCE_FIELD(bool, isVisible);
+
+    DECLARE_INSTANCE_METHOD(bool, get_isVisible);
+    DECLARE_INSTANCE_METHOD(void, set_isVisible, bool value);
+
+    DECLARE_INSTANCE_METHOD(StringW, get_tabName);
+    DECLARE_INSTANCE_METHOD(void, set_tabName, StringW value);
+
+    DECLARE_INSTANCE_METHOD(StringW, get_tabKey);
+    DECLARE_INSTANCE_METHOD(void, set_tabKey, StringW value);
+
+    DECLARE_CTOR(ctor);
+)
