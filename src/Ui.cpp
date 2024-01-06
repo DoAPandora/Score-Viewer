94a8505b2476efe8f1482339700af990f9102b10
DoA Voices <100880439+DoAPandora@users.noreply.github.com>
2024-01-05 21:28:26 -0500

    Bro Why

--- src/Ui.cpp
+++ src/Ui.cpp
@@ -9,6 +9,6 @@ void DidActivate(HMUI::ViewController* self, bool firstActivation, bool addedToH
     // Create our UI elements only when shown for the first time.
     if(firstActivation) {
         
-        BSML::ClickableImage* CreateClickableImage(const TransformWrapper& parent, UnityEngine::Sprite* sprite, std::function<void()> onClick = nullptr, UnityEngine::Vector2 anchoredPosition = {-70, 30}, UnityEngine::Vector2 sizeDelta = {9.0f, 9.0f});
+        BSML::ClickableImage* CreateClickableImage(const TransformWrapper& parent, UnityEngine::Sprite* sprite, std::function<void()> onClick = nullptr, UnityEngine::Vector2 anchoredPosition = {-70, 30}, UnityEngine::Vector2 sizeDelta = {9.0f, 9.0f}
     }
 }
