fca3f27cc8c2e6f2c2c464603142ad2fb9af0821
DoA Voices <100880439+DoAPandora@users.noreply.github.com>
2024-02-10 15:17:01 -0500

    Finishing Up

--- include/config.hpp
+++
@@ -1,4 +0,0 @@
-#include "main.hpp"
-#include "config-utils/shared/config-utils.hpp"
-
-    auto object = ::QuestUI::BeatSaberUI::CreateToggle(parent, configValue.GetName(), configValue.GetValue()),
