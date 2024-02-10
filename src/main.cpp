8134b937611d1f0969eb261ff6fd507442c05f7a
DoA Voices <100880439+DoAPandora@users.noreply.github.com>
2024-02-10 15:27:05 -0500

    Add files via upload

--- src/main.cpp
+++ src/main.cpp
@@ -7,35 +7,17 @@
 
 #include "GlobalNamespace/LevelListTableCell.hpp"
 
+#include "UnityEngine/Color.hpp"
 #include "UnityEngine/GameObject.hpp"
 #include "UnityEngine/Transform.hpp"
 
+#include "HMUI/ImageView.hpp"
+
+#include "TMPro/TMP_Text.hpp"
+
+#include "Polyglot/LocalizedTextMeshProUGUI.hpp"
+
 #include "song-details/shared/SongDetails.hpp"
-
-static ModInfo modInfo; // Stores the ID and version of our mod, and is sent to the modloader upon startup
-
-// Loads the config from disk using our modInfo, then returns it for use
-// other config tools such as config-utils don't use this config, so it can be removed if those are in use
-Configuration& getConfig() {
-    static Configuration config(modInfo);
-    return config;
-}
-
-// Returns a logger, useful for printing debug messages
-Logger& getLogger() {
-    static Logger* logger = new Logger(modInfo);
-    return *logger;
-}
-
-// Called at the early stages of game loading
-extern "C" void setup(ModInfo& info) {
-    info.id = MOD_ID;
-    info.version = VERSION;
-    modInfo = info;
-	
-    getConfig().Load();
-    getLogger().info("Completed setup!");
-}
 
 SongDetailsCache::SongDetails* songDetails;
 
@@ -115,6 +97,25 @@ Logger &getLogger()
 {
     static Logger *logger = new Logger(modInfo);
     return *logger;
+}
+
+static ModInfo modInfo; // Stores the ID and version of our mod, and is sent to the modloader upon startup
+
+// Loads the config from disk using our modInfo, then returns it for use
+// other config tools such as config-utils don't use this config, so it can be removed if those are in use
+Configuration& getConfig() {
+    static Configuration config(modInfo);
+    return config;
+}
+
+// Called at the early stages of game loading
+extern "C" void setup(ModInfo& info) {
+    info.id = MOD_ID;
+    info.version = VERSION;
+    modInfo = info;
+	
+    getConfig().Load();
+    getLogger().info("Completed setup!");
 }
 
 // Called later on in the game loading - a good time to install function hooks
