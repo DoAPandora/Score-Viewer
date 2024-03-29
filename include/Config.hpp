bb0e0ce45b157d3052ab9ba2b0261e30af87cf1b
DoA Voices <100880439+DoAPandora@users.noreply.github.com>
2024-03-29 16:33:32 -0400

    Update To 1.35.0

--- include/Config.hpp
+++
@@ -1,7 +0,0 @@
-#include "config-utils/shared/config-utils.hpp"
-
-DECLARE_CONFIG(ModConfig,
-    CONFIG_VALUE(Enabled, bool, "Enabled", true);
-    CONFIG_VALUE(DifferentText, bool, "Different Text", true);
-    CONFIG_VALUE(DifferentColor, bool, "Different Color", true);
-)
