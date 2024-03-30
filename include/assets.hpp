d2cf825b9d512c5a4584fe04064a8acbc00df1ca
DoA Voices <100880439+DoAPandora@users.noreply.github.com>
2024-03-30 13:02:33 -0400

    Delete include/assets.hpp

--- include/assets.hpp
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include "kaleb/shared/kaleb.hpp"
-
-DECLARE_FILE(_binary_SettingsView_bsml, Assets, Settings_bsml);
-DECLARE_FILE(_binary_AdvancedSettingsView_bsml, Assets, AdvancedSettings_bsml);
