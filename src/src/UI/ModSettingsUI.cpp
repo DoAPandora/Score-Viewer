1900933a38baee03ef3a05aa96e7902c6da5d07e
DoA Voices <100880439+DoAPandora@users.noreply.github.com>
2024-03-30 13:10:18 -0400

    Update To 1.35.0

---
+++ src/src/UI/ModSettingsUI.cpp
@@ -0,0 +1,9 @@
+#include "bsml/shared/BSML.hpp"
+#include "UI/ScoreViewerFlowCoordinator.hpp"
+
+        static inline bool RegisterSettingsMenu(std::string_view ScoreViewer, bool enableExtraButtons = false) {
+            return RegisterSettingsMenu(ScoreViewer, csTypeOf(T), MenuSource::ViewController, enableExtraButtons);
+        }
+        
+        bool RegisterSettingsMenu(std::string_view ScoreViewer, std::function<void(HMUI::ViewController*, bool, bool, bool)> viewControllerDidActivate, bool enableExtraButtons);
+        
